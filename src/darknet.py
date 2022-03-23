ddf4119ae34f044b76a4385b1f7f72631e8c50dc
AddictionLord <marousiinek@gmail.com>
2022-03-23 15:24:20 +0100

    Darknet53 feature detector initialized

---
+++ src/darknet.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn
+
+
+'''
+Darknet53 feature detector from Yolov3 object detection model
+config file: https://github.com/pjreddie/darknet/blob/master/cfg/darknet53.cfg
+
+'''
+
+
+
+class Darknet(nn.Module):
+    def __init__(self):
+
+        pass
+
+
+# ------------------------------------------------------
+def forward(self, x):
+
+    pass
+
+
+
+
+
+
+
+
+
+
+if "__name__" == __main__:
+
+    pass
