7fe3f4ac04f85cf5765ffdb1f74f4ce3ff682df6
AddictionLord <marousiinek@gmail.com>
2022-03-29 17:51:50 +0200

    ScalePrediction block finished

--- src/blocks/scale_prediction_block.py
+++ src/blocks/scale_prediction_block.py
@@ -7,6 +7,8 @@ sys.path.insert(1, '/home/mary/thesis/project/src/')
 from blocks.cnn_block import CNNBlock
 
 
+
+
 # Scale predictior outputs:
 #   - number of channels = 3 * (N + 5)
 #       --> 3 anchor boxes
@@ -18,13 +20,21 @@ class ScalePrediction(nn.Module):
 
         self.num_of_classes = num_of_classes
         self.block = nn.Sequential(
-            CNNBlock(in_channels, in_channels * 2),
-            CNNBlock(in_channels * 2, 3 * (num_of_classes + 5))            
+            CNNBlock(in_channels, in_channels * 2, kernel_size=3, padding=1),
+            CNNBlock(in_channels * 2, 3 * (num_of_classes + 5), batch_norm=False, kernel_size=1)
         )
 
 
     # ------------------------------------------------------
+    # W - Width, H - Height, B - batch_size, N - number of classes, A - number of anchor boxes
+    # self.block(x) returns shape (B, A * (N + 5), W, H)
+    # reshape it to (B, A, N + 5, W, H)
+    # permute to (B, A, W, H, N + 5)
+    # Doesn't really matter, format just needs to be consistent (for loss etc..)
     def forward(self, x):
 
-        return self.block(x)
+        return self.block(x).reshape(
+            x.shape[0], 3, self.num_of_classes + 5, x.shape[2], x.shape[3]).permute(
+                0, 1, 3, 4, 2
+            )
 
