f4a0a7d15b07f170df267c32fa9c3c3b82f6c05d
AddictionLord <marousiinek@gmail.com>
2022-03-30 18:24:03 +0200

    Class CNNBuilder finished - v1.0

--- src/blocks/cnn_builder.py
+++ src/blocks/cnn_builder.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
 
-from blocks.cnn_block import CNNBlock
+from cnn_block import CNNBlock
 from blocks.residual_block import ResidualBlock
 from blocks.scale_prediction_block import ScalePrediction
-
 
 
 darknet_config = [
@@ -20,25 +19,52 @@ darknet_config = [
     (1024, 3, 2),
     ["R", 4]
 ]
-
-yolo_config = [
+'''
+Set: {"U", 2} - means upsampling layer, scale_factor=2
+List: ["R", 8] - means residual layer, number_of_repeats=8
+      ["C", 8] - means convolutinal layers, number_of_repeats=8
+Strign: "S" - means scale prediction layer
+'''
+yolo = [
+    (32, 3, 1),
+    (64, 3, 2),
+    ["R", 1],
+    (128, 3, 2),
+    ["R", 2],
+    (256, 3, 2),
+    ["R", 8],
+    (512, 3, 2),
+    ["R", 8],
+    (1024, 3, 2),
+    ["R", 4], #darknet53 to this point
     (512, 1, 1),
     (1024, 3, 1),
+    ["C", 1],
+    (512, 1, 1),
     "S",
     (256, 1, 1),
-    "U",
+    {"U", 2},
     (256, 1, 1),
     (512, 3, 1),
+    ["C", 1],
+    (256, 1, 1),
     "S",
     (128, 1, 1),
-    "U",
+    {"U", 2},
     (128, 1, 1),
     (256, 3, 1),
+    ["C", 1],
+    (128, 1, 1),
     "S"
 ]
 
-
 class CNNBuilder:
+    def __init__(self, in_channels, num_of_classes, config=yolo):
+
+        self.in_channels = in_channels
+        self.num_of_classes = num_of_classes
+        self.config = config
+
 
     # ------------------------------------------------------
     def _constructNeuralNetwork(self, config):
@@ -47,23 +73,27 @@ class CNNBuilder:
         net = nn.ModuleList()
         for block in self.config:
 
-            # Construction of CNNBlock and integration to network
-            # CNNBlock changes number of channels - update:
+            # Construction of CNNBlock
+            # CNNBlock changes number of channels, thus update
             if isinstance(block, tuple):
-                out_channels, kernel_size, stride = block
-                layer = self._buildCNNLayer(in_channels, block)
-                in_channels = out_channels
+                layer, in_channels = self._buildCNNLayer(in_channels, block)
 
-            # Construction of ResidualBlock and integration to network
+            # Construction of ResidualBlock
             # ResidualBlock doesn't change number of channels (no update needed)
             elif isinstance(block, list):
-                block_type, num_of_repeats = block
                 layer = self._buildResidualLayer(in_channels, block)
+
+            # Construction of ScalePrediction block
+            elif isinstance(block, str):
+                layer = self._buildScalePredictionLayer(in_channels, self.num_of_classes)
+
+            # Construction of Upsampling block
+            elif isinstance(block, set):
+                layer, in_channels = self._buildUpsampleLayer(in_channels, block)
 
             net.append(layer)
 
         return net
-
 
 
     # ------------------------------------------------------
@@ -78,23 +108,45 @@ class CNNBuilder:
             padding=1 if kernel_size == 3 else 0
         )
 
-        return layer
+        return layer, out_channels
 
 
     # ------------------------------------------------------
+    # block_type == 'R' builds block with residual behaviour,
+    # block_type == 'C' just stacks CNNBlock
     def _buildResidualLayer(self, in_channels, block):
 
         block_type, num_of_repeats = block
         layer = ResidualBlock(
             num_of_repeats, 
-            in_channels
+            in_channels,
+            residual=True if block_type == 'R' else False
         )
 
         return layer
 
 
-    def _buildScalePredictionLayer(self, in_channels, block):
+    # ------------------------------------------------------
+    def _buildScalePredictionLayer(self, in_channels, num_of_classes):
 
-        pass
+        return ScalePrediction(in_channels, self.num_of_classes)
 
 
+    # ------------------------------------------------------
+    def _buildUpsampleLayer(self, in_channels, block):
+
+        scale_factor, _ = block
+
+        return nn.Upsample(scale_factor=scale_factor), in_channels * 3
+
+
+
+
+if __name__ == '__main__':
+
+    b = CNNBuilder(3, 6)
+    net = b._constructNeuralNetwork(yolo)
+
+    for block in net:
+
+        print(type(block))
