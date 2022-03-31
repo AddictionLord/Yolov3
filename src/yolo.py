440977fb5851849dc8d60e33b9597704c5290a7f
AddictionLord <marousiinek@gmail.com>
2022-03-31 19:32:50 +0200

    Forward method implemented, Yolo construction call modified, test fcns implemented + passed, yolo - v1.0

--- src/yolo.py
+++ src/yolo.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 
 from darknet import Darknet
+from blocks.scale_prediction_block import ScalePrediction
 from blocks.cnn_builder import CNNBuilder
+
 
 
 '''
@@ -18,42 +20,21 @@ architecture inspirations:
 config = [
     (512, 1, 1),
     (1024, 3, 1),
-    "S",
-    (256, 1, 1),
-    "U",
-    (256, 1, 1),
-    (512, 3, 1),
-    "S",
-    (128, 1, 1),
-    "U",
-    (128, 1, 1),
-    (256, 3, 1),
-    "S"
-]
-
-
-config = [
-    (512, 1, 1),
-    (1024, 3, 1),
-    ["R", 1],
+    ["C", 1],
     (512, 1, 1),
     "S",
-
     (256, 1, 1),
-    "U",
-
+    {"U": 2},
     (256, 1, 1),
     (512, 3, 1),
-    ["R", 1],
+    ["C", 1],
     (256, 1, 1),
     "S",
-
     (128, 1, 1),
-    "U",
-
+    {"U": 2},
     (128, 1, 1),
     (256, 3, 1),
-    ["R", 1],
+    ["C", 1],
     (128, 1, 1),
     "S"
 ]
@@ -68,45 +49,79 @@ class Yolov3(nn.Module, CNNBuilder):
         self.num_of_classes = num_of_classes
 
         self.darknet = Darknet(in_channels, pretrained=True)
-        self.yolo = self._constructNeuralNetwork(config)
+        self.yolo = self._constructNeuralNetwork(config, self.darknet.out_channels)
 
 
     # ------------------------------------------------------
     def forward(self, x):
 
+        outputs = list()
         x = self.darknet(x)
         for layer in self.yolo:
 
+            # print(x.shape)
+            print(type(layer))
+            # print(layer.block)
+
+            # Scale prediction outputs prediction tensor, but don't 
+            # influence next layers, thus continuation after
+            if isinstance(layer, ScalePrediction):
+                outputs.append(layer(x))
+                continue
+
             x = layer(x)
 
-        return x
+            # After upsampling layer output is concatenated with
+            # tensor from darknet residual layer - route connection
+            if isinstance(layer, nn.Upsample):
+                x = torch.cat((x, self.darknet.getTensorToConcatenate()), dim=1)
+
+        return outputs
 
 
-                
 
-
-
-
-
-if __name__ == "__main__":
-
+# ------------------------------------------------------
+# Testing functions
+# ------------------------------------------------------
+def showYoloBlocks():
+        
     from blocks.cnn_block import CNNBlock
     from blocks.residual_block import ResidualBlock
 
     y = Yolov3(config)
     num_of_blocks = len(y.yolo)
-    num_of_layers = 0
     print(f'Number of blocks: {num_of_blocks}')
 
     for index, block in enumerate(y.yolo):
 
         print(type(block))
-        if isinstance(block, ResidualBlock):
-            # print(len(block.block))
-            num_of_layers += len(block.block)
 
-        else:
-            num_of_layers += 1
 
-    print(num_of_layers)
-    
+def testYoloOutputSize():
+
+    num_of_classes = 6
+    model = Yolov3(config, 3, num_of_classes)
+
+    BATCH_SIZE = 1
+    INPUT_WIDTH = 256
+    INPUT_HEIGHT = 256
+    t = torch.rand(BATCH_SIZE, 3, INPUT_WIDTH, INPUT_HEIGHT)
+
+    out = model(t)
+
+    # assert out[0].shape == (BATCH_SIZE, 1024, INPUT_WIDTH // 32, INPUT_HEIGHT // 32, num_of_classes + 5)
+    # assert out[1].shape == (BATCH_SIZE, 1024, INPUT_WIDTH // 16, INPUT_HEIGHT // 16, num_of_classes + 5)
+    # assert out[2].shape == (BATCH_SIZE, 1024, INPUT_WIDTH // 8, INPUT_HEIGHT // 8, num_of_classes + 5)
+    print('Test was successful - Image output size is correct!')
+
+
+
+# ------------------------------------------------------
+# Main - mostly for testing purposes
+# ------------------------------------------------------
+if __name__ == "__main__":
+
+    # showYoloBlocks()
+    testYoloOutputSize()
+
+
