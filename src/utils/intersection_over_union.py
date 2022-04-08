bcde1e6851b6ba06b528280e3b59cf4d068ee21e
AddictionLord <marousiinek@gmail.com>
2022-04-08 22:54:45 +0200

    Fcn intersectionOverUnion implemented

--- src/utils/intersection_over_union.py
+++ src/utils/intersection_over_union.py
@@ -2,6 +2,7 @@ import torch
 import sys
 
 sys.path.insert(1, '/home/mary/thesis/project/src/')
+from utils import BoundingBox
 import config
 
 
@@ -18,25 +19,91 @@ def iouBetweenBboxAnchor(bbox, anchor):
     return intersection / union
 
 
+# ------------------------------------------------------
+# Computes intersection, union and returns intersection/union
+# labels/pred in format midpoint: [x, y, w, h], corners: [x1, y1, x2, y2]
+def intersectionOverUnion(
+    preds: torch.tensor, labels: torch.tensor, midpoint_format=True
+):
+
+    preds = BoundingBox(preds, midpoint=midpoint_format)
+    labels = BoundingBox(labels, midpoint=midpoint_format)
+
+    if midpoint_format:
+        preds.toCornersForm()
+        labels.toCornersForm()
+
+    x1 = torch.max(preds.x1, labels.x1)
+    y1 = torch.max(preds.y1, labels.y1)
+    x2 = torch.min(preds.x2, labels.x2)
+    y2 = torch.min(preds.y2, labels.y2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+    box1_area = abs((preds.x2 - preds.x1) * (preds.y2 - preds.y1))
+    box2_area = abs((labels.x2 - labels.x1) * (labels.y2 - labels.y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
 
 
+
+# def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
+
+#     if box_format == "midpoint":
+#         box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+#         box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+#         box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+#         box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+#         box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+#         box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+#         box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+#         box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+
+#     if box_format == "corners":
+#         box1_x1 = boxes_preds[..., 0:1]
+#         box1_y1 = boxes_preds[..., 1:2]
+#         box1_x2 = boxes_preds[..., 2:3]
+#         box1_y2 = boxes_preds[..., 3:4]
+#         box2_x1 = boxes_labels[..., 0:1]
+#         box2_y1 = boxes_labels[..., 1:2]
+#         box2_x2 = boxes_labels[..., 2:3]
+#         box2_y2 = boxes_labels[..., 3:4]
+
+#     x1 = torch.max(box1_x1, box2_x1)
+#     y1 = torch.max(box1_y1, box2_y1)
+#     x2 = torch.min(box1_x2, box2_x2)
+#     y2 = torch.min(box1_y2, box2_y2)
+
+#     intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+#     box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+#     box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+#     return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+
+# ------------------------------------------------------
 if __name__ == '__main__':
 
-    l = [1, 2, 3, 4, 5]
-    print(l[2:4])
+    # l = [1, 2, 3, 4, 5]
+    # print(l[2:4])
 
-    t = torch.eye(1, 2)
-    print(t)
-    print(t[:, 0])
+    # t = torch.eye(1, 2)
+    # print(t)
+    # print(t[:, 0])
 
-    anchors = config.ANCHORS
-    anch =  torch.tensor(anchors[0] + anchors[1] + anchors[2], dtype=torch.float64)
-    print(anch)
+    # anchors = config.ANCHORS
+    # anch =  torch.tensor(anchors[0] + anchors[1] + anchors[2], dtype=torch.float64)
+    # print(anch)
 
-    sort = torch.argsort(anch, dim=1)
-    print(sort)
+    # sort = torch.argsort(anch, dim=1)
+    # print(sort)
 
-    box = [1, 2, 3, 4, 5]
-    iou = iouBetweenBboxAnchor(torch.tensor(box[2:4]), anch)
+    # box = [1, 2, 3, 4, 5]
+    # iou = iouBetweenBboxAnchor(torch.tensor(box[2:4]), anch)
 
-    
+    preds = torch.tensor([2, 2, 2, 4])
+    labels = torch.tensor([20, 20, 3, 4])    
+    iou1 = intersectionOverUnion(preds, labels)
+    # iou2 = intersection_over_union(preds, labels)
+
+    print(iou1)
+    # print(iou2)
