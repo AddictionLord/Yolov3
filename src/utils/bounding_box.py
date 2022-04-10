6f646c25f1a74d5265d7504e08184c1322deb600
AddictionLord <marousiinek@gmail.com>
2022-04-10 23:26:12 +0200

    BoundingBox class modified for wider use

--- src/utils/bounding_box.py
+++ src/utils/bounding_box.py
@@ -7,68 +7,104 @@ https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pyt
 
 
 # ------------------------------------------------------
-# TODO: Clean the constructor, make some format handling private fcns
+# formats: 'midpoint': [x, y, w, h]
+#          'corners':  [x1, y1, x2, y2]
+#          'coco':     [top_left_x, top_left_y, w, h]
+# MSCOCO - 'coco' format is allowed only for loading (from dictionary)
+# 
+# lengths/representations: [x, y, w, h, class], [class, prob, x, y, w, h]
 class BoundingBox:
-    def __init__(self, 
-        bbox: [torch.tensor, list], 
-        midpoint=True, 
-        anchors:torch.tensor = None
-    ):
+    def __init__(self, bbox: [torch.tensor, list, dict], form='midpoint'):
 
+        self.form = form
+
+        bbox = self._parseAnnotations(bbox) if isinstance(bbox, dict) else bbox
         bbox = torch.tensor(bbox) if isinstance(bbox, list) else bbox
-        # tensor shape: [BATCH, A, S, S, 6]
-        if len(bbox.shape) == 5:
-            self._fromCellsTensor(bbox, anchors)
 
-        # this is for format bbox: [x, y, w, h, class]
-        elif bbox.shape[0] == 5:
+        bbox = self._sliceBbox(bbox)
+        self._initForm(bbox)
+
+
+    # ------------------------------------------------------
+    # Slice bbox according to allowed representation
+    def _sliceBbox(self, bbox: torch.tensor):
+
+        if len(bbox.shape) > 1:
+            rows, cols = bbox.shape[0], bbox.shape[1] 
+        else:
+            rows, cols = bbox.shape[0], None 
+
+        # this is for bbox repre: [x, y, w, h]
+        if rows == 4 or cols == 4:
+            bbox = bbox.view(-1, 4)
+
+        # this is for bbox repre: [x, y, w, h, class]
+        elif rows == 5 or cols == 5:
             self.classification = bbox[..., 4]
             bbox = bbox[..., 0:4].view(-1, 4)
 
-        # this is for format bbox: [class, prob, x, y, w, h]
-        elif bbox.shape[0] == 6:
+        # this is for bbox repre: [class, prob, x, y, w, h]
+        elif rows == 6 or cols == 6:
             self.classification = bbox[..., 0]
             self.probability = bbox[..., 1]
             bbox = bbox[..., 2:6].view(-1, 4)
 
-        if midpoint:
-            self.midpoint, self.corners = bbox, None
-            self.x, self.y = self.midpoint[..., 0:1], self.midpoint[..., 1:2]
-            self.w, self.h = self.midpoint[..., 2:3], self.midpoint[..., 3:4]
-
         else:
-            self.midpoint, self.corners = None, bbox
-            self.x1, self.y1 = self.corners[..., 0:1], self.corners[..., 1:2]
-            self.x2, self.y2 = self.corners[..., 2:3], self.corners[..., 3:4]
+            raise("[BoundingBox]: Unknow representation, please use allowed")
+
+        return bbox
 
 
     # ------------------------------------------------------
-    # celss: [BATCH, A, S, S, 6] -> 6: [score, x, y, w, h, classification]
-    def _fromCellsTensor(self, cells: torch.tensor, anchors):
+    def toForm(self, form: str):
 
-        S = cells.shape[2]
-        batch = cells.shape[0]
-        score = cells[..., 0:1]
+        if form == 'coco':
+            raise('[BoundingBox]: Coco format is only for loading, use midpoint/corners')
 
-        if anchors is not None:
-            num_of_anchors = len(anchors)
-            anchors = anchors.reshape(1, num_of_anchors, 1, 1, 2)
-            cells[..., 0:2] = torch.sigmoid(cells[..., 0:2])
-            cells[..., 3:5] = torch.exp(cells[..., 3:5]) * anchors ##Checkpoint
-            score = torch.sigmoid(score)
-            best_class = torch.argmax(cells[..., 5:], dim=-1).unsqueeze(-1)
+        elif form == 'midpoint':
+            self.form = 'midpoint'
+            return self.toMidpointForm()
 
-        # cell_indices: [1, 3, 13, 13, 1]
-        cell_indices = torch.arange(S).repeat(batch, 3, S, 1).unsqueeze(-1).to(cells.device)
-        x = 1 / S * (cells[..., 1:2] + cell_indices)
-        y = 1 / S * (cells[..., 2:3] + cell_indices.permute(0, 1, 3, 2, 4))
-        wh = 1 / S * (cells[..., 3:5])
+        elif form == 'corners':
+            self.form = 'corners'
+            return self.toCornersForm()
 
-        self.bboxes = torch.cat((best_class, score, x, y, wh), dim=-1).reshape(
-            batch, num_of_anchors * S * S, 6
-        )
-        
-        return self.bboxes.tolist()
+        else:
+            raise('[BoundingBox]: Unknow format, use midpoint/corners')
+            
+
+    # ------------------------------------------------------
+    # formats: 'midpoint': [x, y, w, h]
+    #          'corners':  [x1, y1, x2, y2]
+    #          'coco':     [top_left_x, top_left_y, w, h]
+    def _initForm(self, bbox: torch.tensor):
+
+        if self.form == 'midpoint':
+            self.bbox = bbox
+            self.x, self.y = self.bbox[..., 0:1], self.bbox[..., 1:2]
+            self.w, self.h = self.bbox[..., 2:3], self.bbox[..., 3:4]
+
+        elif self.form == 'corners':
+            self.bbox = bbox
+            self.x1, self.y1 = self.bbox[..., 0:1], self.bbox[..., 1:2]
+            self.x2, self.y2 = self.bbox[..., 2:3], self.bbox[..., 3:4]
+
+        elif self.form == 'coco':
+            self.form = 'midpoint'
+            self.w, self.h = bbox[..., 2:3], bbox[..., 3:4]  
+            self.x = bbox[..., 0:1] + (self.w / 2)
+            self.y = bbox[..., 1:2] + (self.h / 2)
+            self.bbox = torch.cat((self.x, self.y, self.w, self.h), dim=1)
+
+
+    # ------------------------------------------------------
+    # Parses Coco annotations
+    def _parseAnnotations(self, anns: dict):
+
+        classification = anns['category_id']
+        bbox = anns['bbox']
+
+        return torch.tensor(bbox.append(classification))
 
 
     # ------------------------------------------------------
@@ -76,52 +112,94 @@ class BoundingBox:
     # [x1, y1] - left top, [x2, y2] - right bottom 
     def toCornersForm(self):
 
-        if self.corners is None:
-            self.x1 = (self.x - self.w / 2)
-            self.y1 = (self.y - self.h / 2)
-            self.x2 = (self.x + self.w / 2)
-            self.y2 = (self.y + self.h / 2)
-            self.corners = torch.cat((self.x1, self.y1, self.x2, self.y2), dim=1)
+        self.x1 = (self.x - self.w / 2)
+        self.y1 = (self.y - self.h / 2)
+        self.x2 = (self.x + self.w / 2)
+        self.y2 = (self.y + self.h / 2)
+        self.bbox = torch.cat((self.x1, self.y1, self.x2, self.y2), dim=1)
 
-        return self.corners
+        return self.bbox
 
 
     # ------------------------------------------------------
     # If bbox is in corners form, this recomputes coords to midpoint form
     def toMidpointForm(self):
 
-        if self.midpoint is None:
-            self.w = (self.x1 - self.x2).abs()
-            self.h = (self.y1 - self.y2).abs()
-            self.x = ((self.x2 - self.x1) / 2 + self.x1)
-            self.y = ((self.y2 - self.y1) / 2 + self.y1)
-            self.midpoint = torch.cat((self.x, self.y, self.w, self.h), dim=1)
+        self.w = (self.x1 - self.x2).abs()
+        self.h = (self.y1 - self.y2).abs()
+        self.x = ((self.x2 - self.x1) / 2 + self.x1)
+        self.y = ((self.y2 - self.y1) / 2 + self.y1)
+        self.bbox = torch.cat((self.x, self.y, self.w, self.h), dim=1)
 
-        return self.midpoint
+        return self.bbox
 
 
+    # # ------------------------------------------------------
+    # # celss: [BATCH, A, S, S, 6] -> 6: [score, x, y, w, h, classification]
+    # def _fromCellsTensor(self, cells: torch.tensor, anchors):
+
+    #     S = cells.shape[2]
+    #     batch = cells.shape[0]
+    #     score = cells[..., 0:1]
+
+    #     if anchors is not None:
+    #         num_of_anchors = len(anchors)
+    #         anchors = anchors.reshape(1, num_of_anchors, 1, 1, 2)
+    #         cells[..., 0:2] = torch.sigmoid(cells[..., 0:2])
+    #         cells[..., 3:5] = torch.exp(cells[..., 3:5]) * anchors ##Checkpoint
+    #         score = torch.sigmoid(score)
+    #         best_class = torch.argmax(cells[..., 5:], dim=-1).unsqueeze(-1)
+
+    #     # cell_indices: [1, 3, 13, 13, 1]
+    #     cell_indices = torch.arange(S).repeat(batch, 3, S, 1).unsqueeze(-1).to(cells.device)
+    #     x = 1 / S * (cells[..., 1:2] + cell_indices)
+    #     y = 1 / S * (cells[..., 2:3] + cell_indices.permute(0, 1, 3, 2, 4))
+    #     wh = 1 / S * (cells[..., 3:5])
+
+    #     self.bboxes = torch.cat((best_class, score, x, y, wh), dim=-1).reshape(
+    #         batch, num_of_anchors * S * S, 6
+    #     )
+        
+    #     return self.bboxes.tolist()
+
+
+# ------------------------------------------------------
+def conversionCornersMidpointTest():
+
+    bboxes = [
+        list([2, 2, 2, 4, 17]),
+        list([[2, 2, 2, 4, 17], [6, 2, 2, 4, 17]]),
+        torch.tensor([[2, 2, 2, 4], [2, 2, 2, 4], [2, 2, 2, 4], [2, 2, 2, 4]])
+    ]
+
+    for bbox in bboxes:
+
+        b = BoundingBox(bbox, 'midpoint')
+        midpoint = b.bbox
+        print(midpoint)
+        corners = b.toCornersForm()
+        print(corners)
+
+        b = BoundingBox(corners, 'corners')
+        midpoint2 = b.toMidpointForm()
+        print(midpoint2)
+
+        b = BoundingBox(midpoint2, 'midpoint')
+        corners2 = b.toCornersForm()
+        print(corners2)
+        print()
+
+        assert corners[0, 1] == corners2[0, 1]
+        assert corners[0, 2] == corners2[0, 2]
+        assert corners[0, 3] == corners2[0, 3]
+
+    print('Conversion test successfully passed')
 
 
 # ------------------------------------------------------
 if __name__ == '__main__':
 
-    # # bbox = list([2, 2, 2, 4])
-    # bbox = list([[2, 2, 2, 4], [6, 2, 2, 4]])
-    # # bbox = torch.tensor([[2, 2, 2, 4], [2, 2, 2, 4], [2, 2, 2, 4], [2, 2, 2, 4]])
-    # b = BoundingBox(bbox, True)
-    # print(b.midpoint)
-    # bbox2 = b.toCornersForm()
-    # print(bbox2)
-
-    # b = BoundingBox(bbox2, False)
-    # bbox3 = b.toMidpointForm()
-    # print(bbox3)
-
-    # b = BoundingBox(bbox3, True)
-    # bbox4 = b.toCornersForm()
-    # print(bbox4)
-
-    # assert bbox2[1, 2] == bbox4[1, 2]
+    conversionCornersMidpointTest()
 
     # print()
     # bbox = list([2, 2, 2, 4, 1])
@@ -145,9 +223,9 @@ if __name__ == '__main__':
 
     #----------------------------------------
 
-    batch = 1
-    S = 13
-    t = torch.arange(S).repeat(batch, 3, S, 1).unsqueeze(-1) #[1, 3, 13, 13, 1]
+    # batch = 1
+    # S = 13
+    # t = torch.arange(S).repeat(batch, 3, S, 1).unsqueeze(-1) #[1, 3, 13, 13, 1]
 
-    print(t)
-    print(t.shape)
+    # print(t)
+    # print(t.shape)
