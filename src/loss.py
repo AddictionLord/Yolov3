7ed0a8a4c95fcec5a2f4f3455eb82628c31ce31e
AddictionLord <marousiinek@gmail.com>
2022-04-20 17:36:11 +0200

    Loss forward fcn finished, test successful

--- src/loss.py
+++ src/loss.py
@@ -6,6 +6,7 @@ from utils import intersectionOverUnion, TargetTensor
 
 '''
 https://towardsdatascience.com/calculating-loss-of-yolo-v3-layer-8878bfaaf1ff
+https://towardsdatascience.com/yolo-v3-explained-ff5b850390f
 
 '''
 
@@ -18,9 +19,10 @@ class Loss(nn.Module):
         self.entropy = nn.CrossEntropyLoss()
         self.sigmoid = nn.Sigmoid()
 
-        self.lambda_class = 1
-        self.lambda_coord = 10 #5
-        self.lambda_obj = 1
+        # self.lambda_coord = 5 #10
+        # self.lambda_noobj = 0.5 #10
+
+        self.lambda_coord = 10
         self.lambda_noobj = 10
 
 
@@ -36,35 +38,113 @@ class Loss(nn.Module):
         Iobj = target[..., 0] == 1
         Inoobj = target[..., 0] == 0
 
-        # box coordinates loss
-        
-
         # loss when there is no object
         noobj_loss = self.bce(predictions[..., 0:1][Inoobj], target[..., 0:1][Inoobj])
+        noobj_loss[torch.isnan(noobj_loss)] = 0
+
+        print(f'No object loss: {noobj_loss}')
+
 
         # loss when there is object
-        preds, anchors = TargetTensor.convertPredsToBoundingBox(predictions, anchors)
-        ious = intersectionOverUnion(preds[..., 1:5], target[..., 1:5]).detach()
-        obj_loss = self.bce(preds[..., 0:1][Iobj], target[..., 0:1][Iobj] * ious)
+        preds, anchors = TargetTensor.convertPredsToBoundingBox(predictions.detach().clone(), anchors)
+        ious = intersectionOverUnion(preds[..., 1:5][Iobj], target[..., 1:5][Iobj]).detach()
+        obj_loss = self.bce(preds[..., 0:1][Iobj], ious * target[..., 0:1][Iobj])
 
-        
+        print(f'Object loss: {obj_loss}')
+
+        # box coordinates loss
+        xy_loss = self.mse(preds[..., 1:3][Iobj], target[..., 1:3][Iobj])
+        # predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3]) 
+        target[..., 3:5] = torch.log(1e-16 + target[..., 3:5] / anchors)
+        wh_loss = self.mse(predictions[..., 3:5][Iobj], target[..., 3:5][Iobj])
+        box_loss = torch.mean(torch.tensor([xy_loss, wh_loss]))
+        # box_loss = self.mse(predictions[..., 1:5][Iobj], target[..., 1:5][Iobj])
+
+
+        print(f'Box loss: {box_loss}')
+
+        # class loss
+        class_loss = self.entropy(predictions[..., 5:][Iobj], target[..., 5:6][Iobj].long().squeeze())
+
+        # loss fcn
+        loss = (self.lambda_coord * box_loss
+            + obj_loss 
+            + self.lambda_noobj * noobj_loss
+            + class_loss
+        )
+
+        return loss
+
+
 
 
 # ------------------------------------------------------
 if __name__ == "__main__":
 
-    bce = nn.BCEWithLogitsLoss()
+    # bce = nn.BCEWithLogitsLoss()
 
-    t = torch.tensor([1., 0., 1., 0., 0., 0.]).reshape(6, 1).repeat(1, 6)
-    o = torch.tensor([1., 0., 1., 1., 0., 0.]).reshape(6, 1).repeat(1, 6)
+    # t = torch.tensor([1., 0., 1., 0., 0., 0.]).reshape(6, 1).repeat(1, 6)
+    # o = torch.tensor([1., 0., 1., 1., 0., 0.]).reshape(6, 1).repeat(1, 6)
 
-    print(o[..., 0:1][True])
+    # print(o[..., 0:1][True])
 
-    loss = bce((o[..., 0:1][True]), (t[..., 0:1][True]))
-    print(loss)
+    # loss = bce((o[..., 0:1][True]), (t[..., 0:1][True]))
+    # print(loss)
 
-    # l = Loss()
+    with torch.no_grad():
 
-    # predictions = torch.rand(1, 3, 13, 13, 11)
-    # target = torch.rand(1, 3, 13, 13, 5)
-    # l(predictions, target, None)
+        for _ in range(10):
+
+            predictions = torch.rand(1, 3, 13, 13, 11)
+            target = torch.rand(1, 3, 13, 13, 6)
+            target[..., 0:1] = 1
+            anchors = torch.rand(3, 2)
+
+            l = Loss()
+            loss = l(predictions.detach().clone(), target.detach().clone(), anchors.detach().clone())
+            print(f"Loss: {loss.shape}")
+
+            # print(yoloss == loss)
+            print(torch.allclose(yoloss, loss))
+
+            break
+
+    # -------------------------------------------
+    # mse = nn.MSELoss()
+    # cnt = 0
+    # for _ in range(100):
+
+    #     i = torch.rand(2, 4)
+    #     t = torch.rand(2, 4)
+
+    #     def my():
+
+    #         a = mse(i[..., 0:2], t[..., 0:2])
+    #         b = mse(i[..., 2:4], t[..., 2:4])
+
+    #         return torch.mean(torch.tensor([a, b]))
+    #         # return (a + b) / 2 
+
+
+    #     mymse = my()
+    #     tmse = mse(i, t)
+
+    #     cnt += torch.allclose(mymse, tmse)
+
+    # print(cnt)
+
+
+
+    # mse = nn.MSELoss() 
+
+    # a = torch.rand(10, 10, dtype=torch.float64)
+    # b = torch.rand(10, 10, dtype=torch.float64)
+
+    # def m(a, b):
+
+    #     c = b - a
+
+    #     return torch.mean(c**2)
+
+    # print(mse(a, b))
+    # print(m(a, b))
