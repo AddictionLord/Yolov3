a2f0144343c31cec917b71ff33e5586783225133
AddictionLord <marousiinek@gmail.com>
2022-04-21 10:21:59 +0200

    Sources added, basic fcn args

--- src/utils/mean_average_precision.py
+++ src/utils/mean_average_precision.py
@@ -5,10 +5,15 @@ sys.path.insert(1, '/home/mary/thesis/project/src/')
 from utils import intersectionOverUnion
 
 
+r'''
+https://jonathan-hui.medium.com/map-mean-average-precision-for-object-detection-45c121a31173
+https://towardsdatascience.com/breaking-down-mean-average-precision-map-ae462f623a52
+'''
+
 
 # ------------------------------------------------------
 # Metrics used to evaluate object detection models
-def meanAveragePrecision():
+def meanAveragePrecision(preds: list, taget: list):
 
     pass
 
