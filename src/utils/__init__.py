c1077de658009689c1de58b9043f8e9671d3e91c
AddictionLord <marousiinek@gmail.com>
2022-04-21 09:33:03 +0200

    Fcn meanAveragePrecision initiated

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,3 +5,4 @@ from .intersection_over_union import iouBetweenBboxAnchor
 from .intersection_over_union import intersectionOverUnion  
 from .non_max_suprression import nonMaxSuppression, softNonMaxSuppression
 from .target_tensor import TargetTensor
+from .mean_average_precision import meanAveragePrecision
---
+++ src/utils/mean_average_precision.py
@@ -0,0 +1,20 @@
+import torch
+import sys
+
+sys.path.insert(1, '/home/mary/thesis/project/src/')
+from utils import intersectionOverUnion
+
+
+
+# ------------------------------------------------------
+# Metrics used to evaluate object detection models
+def meanAveragePrecision():
+
+    pass
+
+
+
+
+if __name__ == '__main__':
+
+    meanAveragePrecision()
