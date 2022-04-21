abf3d2546d4bdbd8bdf640b93835662062fed10f
AddictionLord <marousiinek@gmail.com>
2022-04-21 16:14:59 +0200

    Base of method trainYoloNet implemented TODO: method _train; fcns: checkClassAccuracy, getBboxesToEvaluate, meanAveragePresicion

--- src/yolo_trainer.py
+++ src/yolo_trainer.py
@@ -22,6 +22,33 @@ class YoloTrainer:
         self.scaled_anchors = config.SCALED_ANCHORS.to(config.DEVICE)
 
 
+    # ------------------------------------------------------
+    # Method to train specific Yolo architecture and return model
+    def trainYoloNet(self, config: list):
+
+        model = Yolov3(config)
+        optimizer = Adam(model.parameters(), config.LEARNING_RATE, weight_decay=config.WEIGHT_DECAY)
+        for epoch in range(config.NUM_OF_EPOCHS):
+
+            self._train(model, optimizer)
+            if epoch != 0 and epoch % 4 == 0:
+                model.eval()
+                # checkClassAccuracy()
+                # preds_bboxes, target_bboxes = getBboxesToEvaluate()
+                # mAP = meanAveragePrecision(preds_bboxes, target_bboxes)
+                model.train()
+
+        return model
+
+    
+    # ------------------------------------------------------
+    def _train(model, optimizer):
+
+        pass
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -29,3 +56,16 @@ if __name__ == '__main__':
     import torch
 
     # t = YoloTrainer()
+    # t.trainYoloNet(config.yolo_config)
+
+    # ------------------------------------------------------
+    # Scaling anchors
+    a = torch.tensor(config.ANCHORS) # shape: [3, 3, 2]
+    S = config.CELLS_PER_SCALE # list len = 3 - need same shape, each matrix a[i, ...] to one scale
+    S = torch.tensor(S).view(-1, 1, 1).repeat(1, 3, 2)
+    scaled_anchors = a * S
+
+    scaled_anchors = torch.tensor(config.ANCHORS) * torch.tensor(config.CELLS_PER_SCALE).view(-1, 1, 1).repeat(1, 3, 2)
+
+
+
