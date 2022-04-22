8d11e711f665d86653e509f5b65b9cbefcc26804
AddictionLord <marousiinek@gmail.com>
2022-04-22 22:38:51 +0200

    Training debugged, working with very slow convergence

--- src/yolo_trainer.py
+++ src/yolo_trainer.py
@@ -1,15 +1,30 @@
 import torch
 from torch.optim import Adam
+import warnings
 from tqdm import tqdm
 
+import config
 from yolo import Yolov3
 from dataset import Dataset
 from loss import Loss
-import config
 
 from utils import getLoaders, TargetTensor
 
+warnings.filterwarnings("ignore")
+torch.backends.cudnn.benchmark = True
 
+'''
+Automatic mixed precision: 
+    https://pytorch.org/docs/stable/amp.html
+    https://pytorch.org/docs/stable/notes/amp_examples.html
+
+Adam optimizer:
+    https://pytorch.org/docs/stable/generated/torch.optim.Adam.html
+
+Progress bar tqdm docs:
+    https://pypi.org/project/tqdm/
+
+'''
 
 
 class YoloTrainer:
@@ -18,10 +33,10 @@ class YoloTrainer:
         self.loss = Loss()
         # self.train_loader, self.val_loader = getLoaders() 
         self.train_loader = getLoaders()
-        self.scaler = torch.cuda.amp.GradScaler()
+        self.scaler = torch.cuda.amp.GradScaler() 
         self.scaled_anchors = config.SCALED_ANCHORS.to(config.DEVICE)
 
-
+    
     # ------------------------------------------------------
     # Method to train specific Yolo architecture and return model
     def trainYoloNet(self, yolo_config: list):
@@ -41,21 +56,29 @@ class YoloTrainer:
 
         return model
 
-    
+
     # ------------------------------------------------------
     def _train(self, model: Yolov3, optimizer: torch.optim):
 
         loader = tqdm(self.train_loader)
+        losses = list()
         for batch, (img, targets) in enumerate(loader):
 
             img = img.to(config.DEVICE)
-            # tar_s1, tar_s2, tar_s3 = TargetTensor.passTargetsToDevice(targets, config.DEVICE)
             targets = TargetTensor.fromDataLoader(self.scaled_anchors, targets)
             with torch.cuda.amp.autocast():
                 output = model(img)
                 loss = targets.computeLossWith(output, self.loss)
-                print(loss)
-                input()
+
+            losses.append(loss.item())
+            optimizer.zero_grad()
+
+            # AMP scaler, vit docs. for more
+            self.scaler.scale(loss).backward()
+            self.scaler.step(optimizer)
+            self.scaler.update()
+
+            loader.set_postfix(loss=torch.mean(torch.tensor(losses)).item())
 
 
 
