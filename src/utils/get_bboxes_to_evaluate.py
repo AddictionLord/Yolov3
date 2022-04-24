7c6b9fb5f1eb4e6d7514eed4e94f56aa1ae176da
AddictionLord <marousiinek@gmail.com>
2022-04-24 22:21:56 +0200

    Fcn getBboxesToEvalueate initiated

---
+++ src/utils/get_bboxes_to_evaluate.py
@@ -0,0 +1,19 @@
+import torch
+
+sys.path.insert(1, '/home/mary/thesis/project/src/')
+import config
+from utils import nonMaxSuppression, TargetTensor
+
+
+
+
+def getBboxesToEvaluate():
+
+    pass
+
+
+
+
+if __name__ == '__main__':
+
+    pass
