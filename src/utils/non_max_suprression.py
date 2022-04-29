ed40fc215a8b008cee4a42fc400316daa8e41e71
AddictionLord <marousiinek@gmail.com>
2022-04-29 15:25:16 +0200

    NMS fcn tested from torch library

--- src/utils/non_max_suprression.py
+++ src/utils/non_max_suprression.py
@@ -3,6 +3,7 @@ import torch
 
 sys.path.insert(1, '/home/mary/thesis/project/src/')
 from utils import intersectionOverUnion
+from torchvision.ops import nms
 
 
 
@@ -75,90 +76,7 @@ def softNonMaxSuppression(
 
     return final
 
-# def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
-#     """
-#     Video explanation of this function:
-#     https://youtu.be/XXYG5ZWtjj0
-#     This function calculates intersection over union (iou) given pred boxes
-#     and target boxes.
-#     Parameters:
-#         boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
-#         boxes_labels (tensor): Correct labels of Bounding Boxes (BATCH_SIZE, 4)
-#         box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
-#     Returns:
-#         tensor: Intersection over union for all examples
-#     """
 
-#     if box_format == "midpoint":
-#         box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
-#         box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
-#         box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
-#         box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
-#         box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
-#         box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
-#         box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
-#         box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
-
-#     if box_format == "corners":
-#         box1_x1 = boxes_preds[..., 0:1]
-#         box1_y1 = boxes_preds[..., 1:2]
-#         box1_x2 = boxes_preds[..., 2:3]
-#         box1_y2 = boxes_preds[..., 3:4]
-#         box2_x1 = boxes_labels[..., 0:1]
-#         box2_y1 = boxes_labels[..., 1:2]
-#         box2_x2 = boxes_labels[..., 2:3]
-#         box2_y2 = boxes_labels[..., 3:4]
-
-#     x1 = torch.max(box1_x1, box2_x1)
-#     y1 = torch.max(box1_y1, box2_y1)
-#     x2 = torch.min(box1_x2, box2_x2)
-#     y2 = torch.min(box1_y2, box2_y2)
-
-#     intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
-#     box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
-#     box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
-
-#     return intersection / (box1_area + box2_area - intersection + 1e-6)
-
-# def non_max_suppression(bboxes, iou_threshold, threshold, box_format="corners"):
-#     """
-#     Video explanation of this function:
-#     https://youtu.be/YDkjWEN8jNA
-#     Does Non Max Suppression given bboxes
-#     Parameters:
-#         bboxes (list): list of lists containing all bboxes with each bboxes
-#         specified as [class_pred, prob_score, x1, y1, x2, y2]
-#         iou_threshold (float): threshold where predicted bboxes is correct
-#         threshold (float): threshold to remove predicted bboxes (independent of IoU)
-#         box_format (str): "midpoint" or "corners" used to specify bboxes
-#     Returns:
-#         list: bboxes after performing NMS given a specific IoU threshold
-#     """
-
-#     assert type(bboxes) == list
-
-#     bboxes = [box for box in bboxes if box[1] > threshold]
-#     bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
-#     bboxes_after_nms = []
-
-#     while bboxes:
-#         chosen_box = bboxes.pop(0)
-
-#         bboxes = [
-#             box
-#             for box in bboxes
-#             if box[0] != chosen_box[0]
-#             or intersection_over_union(
-#                 torch.tensor(chosen_box[2:]),
-#                 torch.tensor(box[2:]),
-#                 box_format=box_format,
-#             )
-#             < iou_threshold
-#         ]
-
-#         bboxes_after_nms.append(chosen_box)
-
-#     return bboxes_after_nms
 
 
 # ------------------------------------------------------
@@ -170,16 +88,30 @@ if __name__ == '__main__':
         [1, 0.9, 1.9, 1.8, 5, 4.5]
     ]
 
-    t = torch.rand((100, 6))
-    bboxes = t.tolist()
+    bboxes = torch.tensor(bboxes)
+    scores = bboxes[..., 1]
+    bboxes = bboxes[..., 2:] / 13
 
-    nms1 = nonMaxSuppression(bboxes, 0.5, 0.5, False)
+    nms_boxes = nms(bboxes, scores, 0.8)
+    print(nms_boxes)
+
+    # print(scores)
+    # print(bboxes)
+
+    
+
+
+    # ------------------------------------------------
+    # t = torch.rand((100, 6))
+    # bboxes = t.tolist()
+
+    # nms1 = nonMaxSuppression(bboxes, 0.5, 0.5, False)
     # soft_nms = softNonMaxSuppression(bboxes, 0.5, 0.5)
 
     # print(f'Non-max suppression:\n{nms}\n')
     # print(f'Soft NMS:\n{soft_nms}')
 
-    print(nms1)
+    # print(nms1)
 
     # difference = list()
     # for _ in range(100):
