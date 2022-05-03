f7ef4a1d603133ee7087c9c8d0af190a2b5f6148
AddictionLord <marousiinek@gmail.com>
2022-05-03 18:46:02 +0200

    Importing also getValLoader to test things

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,4 +6,5 @@ from .non_max_suprression import nonMaxSuppression, softNonMaxSuppression
 from .target_tensor import TargetTensor
 from .mean_average_precision import meanAveragePrecision
 from .get_loaders import getLoaders
+from .get_loaders import getValLoader
 from .get_bboxes_to_evaluate import getBboxesToEvaluate
