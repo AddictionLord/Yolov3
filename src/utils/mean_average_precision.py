e827a72c4120cc8ba86b22c0126c9b3069e136aa
AddictionLord <marousiinek@gmail.com>
2022-05-04 18:44:04 +0200

    Fcn to convert data to mAP implemented

--- src/utils/mean_average_precision.py
+++ src/utils/mean_average_precision.py
@@ -13,39 +13,43 @@ https://towardsdatascience.com/breaking-down-mean-average-precision-map-ae462f62
 
 # ------------------------------------------------------
 # Metrics used to evaluate object detection models
-def convertDataToMAP(preds: list, target: list):
+# Accepts tensors with shape[-1] = 6: [class, score, x, y, w, h]
+def convertDataToMAP(preds: torch.tensor, targets: torch.tensor):
 
     preds = [
         dict(
-            boxes=torch.tensor([[258.0, 41.0, 606.0, 285.0]]),
-            scores=torch.tensor([0.536]),
-            labels=torch.tensor([0]),
+            boxes=preds[..., 2:6].to(torch.float32),
+            scores=preds[..., 1].to(torch.float32),
+            labels=preds[..., 0].to(torch.int32),
         )
     ]
-    target = [
+    targets = [
         dict(
-            boxes=torch.tensor([[214.0, 41.0, 562.0, 285.0]]),
-            labels=torch.tensor([0]),
+            boxes=targets[..., 2:6].to(torch.float32),
+            labels=targets[..., 0].to(torch.int32),
         )
     ]
 
+    return preds, targets
 
 
 
 
+
+# ------------------------------------------------------
 if __name__ == '__main__':
 
     preds = [
         dict(
-            boxes=torch.tensor([[258.0, 41.0, 606.0, 285.0]]),
-            scores=torch.tensor([0.536]),
-            labels=torch.tensor([0]),
+            boxes=torch.tensor([[258.0, 41.0, 606.0, 285.0], [258.0, 41.0, 606.0, 285.0]]),
+            scores=torch.tensor([0.536, 0.536]),
+            labels=torch.tensor([0, 0]),
         )
     ]
     target = [
         dict(
-            boxes=torch.tensor([[214.0, 41.0, 562.0, 285.0]]),
-            labels=torch.tensor([0]),
+            boxes=torch.tensor([[214.0, 41.0, 562.0, 285.0], [214.0, 41.0, 562.0, 285.0]]),
+            labels=torch.tensor([0, 0]),
         )
     ]
 
