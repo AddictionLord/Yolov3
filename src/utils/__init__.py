9f23ce0b21577060ede9346423044f71a83727d0
AddictionLord <marousiinek@gmail.com>
2022-05-08 15:29:01 +0200

    Fcn intersectionOverUnion tested with torch iou fcn

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,6 +7,7 @@ from .weights_handler import WeightsHandler
 from .bounding_box import BoundingBox
 from .intersection_over_union import iouBetweenBboxAnchor
 from .intersection_over_union import intersectionOverUnion  
+from .intersection_over_union import iou  
 from .non_max_suprression import nonMaxSuppression, softNonMaxSuppression
 from .target_tensor import TargetTensor
 from .mean_average_precision import convertDataToMAP
